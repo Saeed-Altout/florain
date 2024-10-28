aa8cb375bd88df77c5641bce3c60396eacbc387f
Saeed Altout <saeedaltoutpro@gmail.com>
2024-10-28 18:13:16 +0300

    refactor(ui): update structure pages.

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,14 +1,16 @@
 import DashboardLayout from "../layouts/DashboardLayout";
 
+import Home from "../pages/Home/home";
+
 import Dashboard from "../pages/Dashboard/Dashboard";
 import Academy from "../pages/Academy/Academy";
 import Analysis from "../pages/Analysis/Analysis";
 import FreeHomes from "../pages/FreeHomes/FreeHomes";
 import NewsFeed from "../pages/NewsFeed/NewsFeed";
-import Home from "../pages/Home/home";
-import Profile from "../pages/Profile/Profile";
-import Details from "../pages/Details/Details";
-import Settings from "../pages/Settings/Settings";
+
+import Profile from "../pages/profile/Profile";
+import Details from "../pages/profile/Details";
+import Settings from "../pages/profile/Settings";
 
 export const routes = [
   {
