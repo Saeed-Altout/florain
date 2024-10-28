358e2566dd84a6e216a938e4b7fa2566acead74c
Saeed Altout <saeedaltoutpro@gmail.com>
2024-10-28 16:56:26 +0300

    feat(ui): add home page and  redirect user to dashboard.

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -5,8 +5,14 @@ import Academy from "../pages/Academy/Academy";
 import Analysis from "../pages/Analysis/Analysis";
 import FreeHomes from "../pages/FreeHomes/FreeHomes";
 import NewsFeed from "../pages/NewsFeed/NewsFeed";
+import Home from "../pages/Home/home";
 
 export const routes = [
+  {
+    path: "/",
+    element: <Home />,
+    children: [],
+  },
   {
     path: "/dashboard",
     element: <DashboardLayout />,
